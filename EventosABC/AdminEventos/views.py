61da5eb63c5d77a45be9423e70db4fcfbf833ce7
jfmatheusg <jfmatheusg@gmail.com>
2020-08-07 02:34:17 -0500

    ajustes para consumo con multipart

--- EventosABC/AdminEventos/views.py
+++ EventosABC/AdminEventos/views.py
@@ -1,18 +1,20 @@
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,parser_classes
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.hashers import make_password
-from django.contrib.auth import authenticate
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from .serializers import *
 from .models import Evento
 
 
 # Create your views here.
 @api_view(['POST'])
+@parser_classes([MultiPartParser, FormParser, JSONParser])
 def CreateUser(request):
+    setattr(request.data, '_mutable', True)
     request.data['password'] = make_password(request.data['password'])
 
     serialized = UserSerializer(data=request.data)
@@ -25,6 +27,7 @@ def CreateUser(request):
 
 class EventView(APIView):
     permission_classes = (IsAuthenticated,)
+    parser_classes = (MultiPartParser, FormParser, JSONParser)
 
     def get(self, request, *args, **kwargs):
         username = Token.objects.get(key=request.auth.key).user
@@ -36,6 +39,7 @@ class EventView(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
 
     def post(self, request, *args, **kwargs):
+
         username = Token.objects.get(key=request.auth.key).user
         userId = User.objects.get(username=username)
         serialized = EventSerializer(data=request.data)
