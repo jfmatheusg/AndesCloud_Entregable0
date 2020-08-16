3104fb5a8f7286a9112c3719da96b10c01903c03
jfmatheusg <jfmatheusg@gmail.com>
2020-08-16 02:35:14 -0500

    pruebas con cors

--- EventosABC/EventosABC/settings.py
+++ EventosABC/EventosABC/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -29,7 +30,9 @@ ALLOWED_HOSTS = ['*']
 
 # CORS
 CORS_ORIGIN_ALLOW_ALL = True
-
+CORS_ALLOW_HEADERS = default_headers + (
+    'Access-Control-Allow-Origin',
+)
 # Application definition
 
 INSTALLED_APPS = [
